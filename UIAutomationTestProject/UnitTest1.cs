d8b88479173b5b58094b5b1f52f7bef6f8013545
Louis Lefevre <llefevre@extron.com>
2023-05-12 10:10:26 +0100

    Add example test

--- UIAutomationTestProject/UnitTest1.cs
+++ UIAutomationTestProject/UnitTest1.cs
@@ -1,3 +1,6 @@
+using FlaUI.Core;
+using FlaUI.Core.Input;
+using FlaUI.UIA3;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UIAutomationTestProject;
@@ -8,5 +11,16 @@ public class ExampleTestClass
     [TestMethod]
     public void ExampleTestMethod()
     {
+        var application = Application.Launch("notepad.exe");
+        var mainWindow = application.GetMainWindow(new UIA3Automation());
+
+        var textEditor = mainWindow.FindFirstDescendant(factory => factory.ByAutomationId("15"));
+        var pattern = textEditor.Patterns.Value.Pattern;
+
+        pattern.SetValue("testing");
+        Wait.UntilInputIsProcessed(TimeSpan.FromSeconds(1));
+        Assert.AreEqual("testing", pattern.Value);
+
+        application.Close();
     }
 }
