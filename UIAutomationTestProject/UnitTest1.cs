634ec9f7783c898728149e88b0cd8cd458a37795
Louis Lefevre <llefevre@extron.com>
2023-05-12 10:30:54 +0100

    Update test

--- UIAutomationTestProject/UnitTest1.cs
+++ UIAutomationTestProject/UnitTest1.cs
@@ -11,16 +11,28 @@ public class ExampleTestClass
     [TestMethod]
     public void ExampleTestMethod()
     {
+        // Open application and get main window.
         var application = Application.Launch("notepad.exe");
         var mainWindow = application.GetMainWindow(new UIA3Automation());
 
+        // Find text editor and get value pattern.
         var textEditor = mainWindow.FindFirstDescendant(factory => factory.ByAutomationId("15"));
         var pattern = textEditor.Patterns.Value.Pattern;
 
+        // Set value of text editor.
         pattern.SetValue("testing");
-        Wait.UntilInputIsProcessed(TimeSpan.FromSeconds(1));
+
+        // Create images directory and take screenshot
+        var dir = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory).Parent!.Parent!.Parent!.Parent!;
+        Wait.UntilInputIsProcessed(TimeSpan.FromSeconds(2));
+        Directory.CreateDirectory(Path.Combine(dir.FullName, "Images"));
+        textEditor.CaptureToFile($"{dir.FullName}/TempImage.png");
+        Wait.UntilInputIsProcessed(TimeSpan.FromSeconds(2));
+
+        // Verify text content is as expected.
         Assert.AreEqual("testing", pattern.Value);
 
+        // Close application.
         application.Close();
     }
 }
